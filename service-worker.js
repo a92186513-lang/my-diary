e79b2ca2fb7b7ccf6dc8d072ac8660f200dd099c
a92186513-lang <a92186513@gmail.com>
2026-09-12 10:10:44 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,113 @@
+const CACHE_NAME = "my-diary-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./db.js",
+  "./auth.js",
+  "./manifest.json"
+];
+
+
+// 설치
+self.addEventListener(
+  "install",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(
+            FILES_TO_CACHE
+          );
+        })
+
+    );
+
+    self.skipWaiting();
+  }
+);
+
+
+// 활성화
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys => {
+
+          return Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          );
+
+        })
+
+    );
+
+    self.clients.claim();
+  }
+);
+
+
+// 파일 요청
+self.addEventListener(
+  "fetch",
+  event => {
+
+    if (
+      event.request.method !== "GET"
+    ) {
+      return;
+    }
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(cache => {
+              cache.put(
+                event.request,
+                copy
+              );
+            });
+
+          return response;
+        })
+
+        .catch(() => {
+
+          return caches.match(
+            event.request
+          );
+
+        })
+
+    );
+
+  }
+);
