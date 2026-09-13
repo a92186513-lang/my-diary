07f65a67c3c19bb7eb6649682ef8975c17fd7bfc
a92186513-lang <a92186513@gmail.com>
2026-09-13 19:31:57 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -593,28 +593,33 @@ async function openEditor(
 
 
         cloudPhotos.push({
+  source:
+    "cloud",
 
-          source:
-            "cloud",
+  name:
+    photo.name,
 
-          name:
-            photo.name,
+  path:
+    photo.path,
 
-          path:
-            photo.path,
+  type:
+    photo.type,
 
-          type:
-            photo.type,
+  size:
+    photo.size,
 
-          size:
-            photo.size,
+  iv:
+    photo.iv,
 
-          url:
-            signedUrl
+  encrypted:
+    photo.encrypted,
 
-        });
+  version:
+    photo.version,
 
-      }
+  url:
+    signedUrl
+});
 
 
       selectedPhotos =
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-diary-v16";
+const CACHE_NAME = "my-diary-v17";
 
 const FILES_TO_CACHE = [
   "./",
